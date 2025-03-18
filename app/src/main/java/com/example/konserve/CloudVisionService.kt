77b7bcc8cfcd8c19010fb7a97033290712239ac6
gokkuu100 <wkurts247@gmail.com>
2025-03-18 19:34:22 +0300

    cloudvision

--- app/src/main/java/com/example/konserve/CloudVisionService.kt
+++ app/src/main/java/com/example/konserve/CloudVisionService.kt
@@ -26,7 +26,7 @@ class CloudVisionService(private val context: Context) {
         private const val TAG = "CloudVisionService"
         private const val MAX_IMAGE_SIZE = 1024 * 1024 // 1MB
         private const val VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate"
-        private const val API_KEY = "AIzaSyAq-MCPT3Dc6Xy-S0ucsi3CB2XVXbUsIbM"
+        private const val API_KEY = ""
     }
 
 
